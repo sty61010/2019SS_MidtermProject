92fe83c7c20f065e8676e8927b0513ff8422967e
曾靖渝 <cengjingyu@cengjingyude-MacBook-Pro.local>
2019-04-24 00:58:24 +0800

    version1

---
+++ js/config.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+/// TODO 1: Change to your firebase config
+///         1. New a Firebase project
+///         2. Copy and paste firebase config below
+var config = {
+    apiKey: "AIzaSyBQ6ha1o5Qwx6keoG3LDf1-vQ8jRwY-oTM",
+    authDomain: "lab06-d3b83.firebaseapp.com",
+    databaseURL: "https://lab06-d3b83.firebaseio.com",
+    projectId: "lab06-d3b83",
+    storageBucket: "lab06-d3b83.appspot.com",
+    messagingSenderId: "715795948333"
+};
+firebase.initializeApp(config);
---
+++ js/signin.js
@@ -0,0 +1,94 @@
+function initApp() {
+    // Login with Email/Password
+    var txtEmail = document.getElementById('inputEmail');
+    var txtPassword = document.getElementById('inputPassword');
+    var btnLogin = document.getElementById('btnLogin');
+    var btnGoogle = document.getElementById('btngoogle');
+    var btnSignUp = document.getElementById('btnSignUp');
+
+    btnLogin.addEventListener('click', function () {
+        /// TODO 2: Add email login button event
+        ///         1. Get user input email and password to login
+        ///         2. Back to index.html when login success
+        ///         3. Show error message by "create_alert" and clean input field
+        
+        firebase.auth().signInWithEmailAndPassword(txtEmail.value, txtPassword.value).then(function (result) {
+            window.location.href = "index.html";            
+        }).catch(function (error) {
+            // Handle Errors here.
+            create_alert("error", "!!!!!!!!!!!");
+        });
+        
+    });
+
+    btnGoogle.addEventListener('click', function () {
+        /// TODO 3: Add google login button event
+        ///         1. Use popup function to login google
+        ///         2. Back to index.html when login success
+        ///         3. Show error message by "create_alert"
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredentialtype that was used.
+            var credential = error.credential;
+        });
+        firebase.auth().signInWithRedirect(provider);
+        firebase.auth().getRedirectResult().then(function (result) {
+            if (result.credential) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // ...
+            }
+            // The signed-in user info.
+            var user = result.user;
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredentialtype that was used.
+            var credential = error.credential;
+        });
+    });
+
+    btnSignUp.addEventListener('click', function () {        
+        /// TODO 4: Add signup button event
+        ///         1. Get user input email and password to signup
+        ///         2. Show success message by "create_alert" and clean input field
+        ///         3. Show error message by "create_alert" and clean input field
+        firebase.auth().createUserWithEmailAndPassword(txtEmail.value, txtPassword.value).catch(function (error) {
+            // Handle Errors here.
+            varerrorCode = error.code;
+            varerrorMessage = error.message;
+        });
+        txtEmail.value = "";
+        txtPassword.value = "";
+    });
+}
+
+// Custom alert
+function create_alert(type, message) {
+    var alertarea = document.getElementById('custom-alert');
+    if (type == "success") {
+        str_html = "<div class='alert alert-success alert-dismissible fade show' role='alert'><strong>Success! </strong>" + message + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+        alertarea.innerHTML = str_html;
+    } else if (type == "error") {
+        str_html = "<div class='alert alert-danger alert-dismissible fade show' role='alert'><strong>Error! </strong>" + message + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+        alertarea.innerHTML = str_html;
+    }
+}
+
+window.onload = function () {
+    initApp();
+};
